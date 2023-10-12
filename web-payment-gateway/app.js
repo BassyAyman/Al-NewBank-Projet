963b96a0535799c1460b959da2c067618cd01bd3
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2023-10-12 18:54:41 +0200

    Create node version of terminal and web gateways

---
+++ web-payment-gateway/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const config = require('./config');
+const port = config.port;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Web Payment Gateway is running');
+});
+
+app.post('/newTransaction', (req, res) => {
+    res.status(201).json("Transaction created");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
