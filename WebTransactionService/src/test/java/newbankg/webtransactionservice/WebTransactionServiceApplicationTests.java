5995ead59c5b30e8d28a36f105679d3ff6020257
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2023-10-15 14:24:29 +0200

    Web Transaction Service initialisation

---
+++ WebTransactionService/src/test/java/newbankg/webtransactionservice/WebTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package newbankg.webtransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
