dee700ea9bf015740cec56763ed4a25f81132572
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-10-15 17:50:36 +0200

    Starting the TerminalTransactionVerificationService

---
+++ TerminalTransactionVerificationService/src/main/java/newbankg/terminaltransactionverificationservice/TerminalTransactionVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package newbankg.terminaltransactionverificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalTransactionVerificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerminalTransactionVerificationServiceApplication.class, args);
+    }
+
+}
---
+++ TerminalTransactionVerificationService/src/test/java/newbankg/terminaltransactionverificationservice/TerminalTransactionVerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package newbankg.terminaltransactionverificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalTransactionVerificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
