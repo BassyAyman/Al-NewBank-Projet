20d61862208f2c640c99ee964073d373252088d5
Tobias Bonifay <90777932+TobiasBonifay@users.noreply.github.com>
2023-11-11 19:54:54 +0100

    Update CreditCardValidator.java
    DEBUG

--- WebTransactionService/src/main/java/newbankg/webtransactionservice/components/cartvalidation/CreditCardValidator.java
+++ WebTransactionService/src/main/java/newbankg/webtransactionservice/components/cartvalidation/CreditCardValidator.java
@@ -4,6 +4,8 @@ import newbankg.webtransactionservice.interfaces.cartbusiness.AlgoCheck;
 import newbankg.webtransactionservice.interfaces.cartbusiness.BINCheck;
 import newbankg.webtransactionservice.interfaces.cartbusiness.ValidateCardValidation;
 import newbankg.webtransactionservice.models.CreditCard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,8 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class CreditCardValidator implements ValidateCardValidation {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreditCardValidator.class);
+
     @Autowired
     BINCheck binCheck;
     @Autowired
@@ -21,16 +25,23 @@ public class CreditCardValidator implements ValidateCardValidation {
     @Override
     public boolean validateCardInTransactionContext(CreditCard cb) {
         String creditCardNumber = cb.getCreditCardNumber();
-        return !isCardExpired(cb.getCreditCartDateExpiration()) //
-                && binCheck.checkCreditCardNumberCoherence(creditCardNumber) //
-                && algoCheck.validateCreditCardAlgoLuhn(creditCardNumber);
+        boolean isExpired = isCardExpired(cb.getCreditCartDateExpiration());
+        boolean isNumberCoherent = binCheck.checkCreditCardNumberCoherence(creditCardNumber);
+        boolean isValidAlgo = algoCheck.validateCreditCardAlgoLuhn(creditCardNumber);
+        
+        LOGGER.debug("Validating credit card number: {}", creditCardNumber);
+        LOGGER.debug("Is card expired: {}", isExpired);
+        LOGGER.debug("Is BIN number coherent: {}", isNumberCoherent);
+        LOGGER.debug("Does card number pass Luhn algorithm check: {}", isValidAlgo);
+
+        return !isExpired && isNumberCoherent && isValidAlgo;
     }
 
     @Override
     public boolean isCardExpired(String expirationDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yy");
-        YearMonth expiry = YearMonth.parse(expirationDate, formatter);
-        LocalDate lastDayOfMonth = expiry.atEndOfMonth();
-        return lastDayOfMonth.isBefore(LocalDate.now());
+        LocalDate expiry = LocalDate.parse(expirationDate, DateTimeFormatter.ofPattern("MM/yy"));
+        boolean isExpired = expiry.isBefore(LocalDate.now());
+        LOGGER.debug("Checking if card is expired. Expiration Date: {}, Is Expired: {}", expirationDate, isExpired);
+        return isExpired;
     }
 }
