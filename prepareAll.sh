4d59a6a178e6749223045ece968080125a1ff7a4
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-11-21 09:41:09 +0100

    interactive mode not supported fix
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.unice.fr>

--- prepareAll.sh
+++ prepareAll.sh
@@ -25,9 +25,20 @@ echo "waiting the database run"
 sleep 5
 # Configure PostgreSQL and restart containers
 echo "Setting up PostgreSQL..."
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "CREATE USER reading_user WITH PASSWORD 'reading_pass';"
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT CONNECT ON DATABASE clientinfo_db TO reading_user;"
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO reading_user;"
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO reading_user;"
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT USAGE ON SCHEMA public TO reading_user;"
-docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO reading_user;"
+# if script is run with parameter "non-interactive"
+if [ "$3" = "--non-interactive" ]; then
+  docker exec postgres-master psql -U postgres -c "CREATE DATABASE clientinfo_db;"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "CREATE USER reading_user WITH PASSWORD 'reading_pass';"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "GRANT CONNECT ON DATABASE clientinfo_db TO reading_user;"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO reading_user;"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO reading_user;"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "GRANT USAGE ON SCHEMA public TO reading_user;"
+  docker exec postgres-master psql -U postgres -d clientinfo_db -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO reading_user;"
+else
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "CREATE USER reading_user WITH PASSWORD 'reading_pass';"
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT CONNECT ON DATABASE clientinfo_db TO reading_user;"
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO reading_user;"
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO reading_user;"
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "GRANT USAGE ON SCHEMA public TO reading_user;"
+  docker exec -it postgres-master psql -U postgres -d clientinfo_db -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO reading_user;"
+fi
