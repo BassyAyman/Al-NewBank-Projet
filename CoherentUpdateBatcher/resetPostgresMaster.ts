cc25ec6365f71756ac5bc39595343384b71bd82b
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2023-12-02 10:45:16 +0100

    Add postgres reset script #10

---
+++ CoherentUpdateBatcher/resetPostgresMaster.ts
@@ -0,0 +1,34 @@
+import {Client} from 'pg';
+
+function resetAllPostgresDebits() {
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'clientinfo_db',
+        port: 5432,
+    });
+
+    client.connect().then(() => {
+        console.log('Connected to Postgres');
+    });
+
+    updateAccountBalances(client);
+}
+
+const updateAccountBalances = async (client: Client) => {
+    try {
+        // update account balance and reset in_debit_amount
+        const query = 'UPDATE account SET amount_money = amount_money - in_debit_amount, in_debit_amount = 0;';
+        const res = await client.query(query);
+
+        console.log('Number of customer accounts updated:', res.rowCount);
+    } catch (err) {
+        console.error('Error while updating accounts', err);
+    } finally {
+        await client.end().then(() => {
+            console.log('Disconnected from Postgres');
+        });
+    }
+};
+
+export default resetAllPostgresDebits;
